19563e24ee2410587b5b85f7a0a621996a58261c
Harsh Agria <113169998+Harsh-Agria@users.noreply.github.com>
2023-05-18 14:31:16 +0530

    pdf_merger.py

--- pdf_merger.py
+++ pdf_merger.py
@@ -1,13 +1,7 @@
 import PyPDF2
 
 pdf_files=["1.pdf","2.pdf","3.pdf"]
-# n=int(input("Enter no. of PDF you want to merge: "))
-# pdf_files=[]
-# print("Enter the PDF name with extension (.pdf)")
-# for i in range(1,n+1):
-#     pdf_files[i]=(input(f"Enter the PDF{i} name : "))
-# print("done")
-
+# Under Improvement
 merger= PyPDF2.PdfMerger()
 for filename in pdf_files:
     pdf_files=open(filename,'rb')
